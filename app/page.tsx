7d353f5c15f36b44d43cbad6fa6ea6c9c3f847cc
LucasNG <84586768+LucasDNG@users.noreply.github.com>
2026-02-01 18:51:41 -0300

    hola amore

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 export default async function Page() {
   console.log("BACKEND_URL:", process.env.BACKEND_URL)
-  const api = process.env.BACKEND_URL
-  
-  const response = await fetch(`${api}/products`);
-  const data = await response.json();
-  console.log(data);
+
+  try {
+    const response = await fetch(`${process.env.BACKEND_URL}/products`, {
+      cache: "no-store",
+    })
+
+    console.log("STATUS:", response.status)
+
+    const data = await response.json()
+    console.log("DATA:", data)
+  } catch (error) {
+    console.error("FETCH ERROR:", error)
+  }
 
   return (
     <div>
-      <h1>Hola amore ! Te amoo </h1>
+      <h1>Hola amore ! Te amoo</h1>
     </div>
-  );
+  )
 }
