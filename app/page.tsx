096492a1919a3e925b0cc60204e64ca93be992b1
LucasNG <84586768+LucasDNG@users.noreply.github.com>
2026-02-01 17:34:56 -0300

    hola amore

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 export default async function Page() {
-  const response = await fetch("http://localhost:3000/products");
+  const api = process.env.BACKEND_URL
+  const response = await fetch(`${api}/products`);
   const data = await response.json();
   console.log(data);
 
   return (
     <div>
-      <h1>Hola amore</h1>
+      <h1>Hola amore ! Te amoo </h1>
     </div>
   );
 }
