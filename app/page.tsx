8627aed07230760d45220b999de4740bdf64fd2f
LucasNG <84586768+LucasDNG@users.noreply.github.com>
2026-02-01 19:13:14 -0300

    hola amore

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
-export const revalidate = 0  // o `export const dynamic = "force-dynamic";`
 
 export default async function Page() {
-  const api = process.env.BACKEND_URL;
 
-  const response = await fetch(`${api}/products`, { cache: "no-store" });
-  const data = await response.json();
-
-  console.log("DATA:", data);
 
   return (
     <div>
       <h1>Hola amore ! Te amoo</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
